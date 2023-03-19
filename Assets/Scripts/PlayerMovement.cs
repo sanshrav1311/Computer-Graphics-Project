2767f82cc949324ec7f5ad9baa69534e9d2f79db
sanshrav1311 <sanshrav1311@gmail.com>
2023-03-19 21:46:59 +0530

    added player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,6 +13,27 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //to add diagnol movement
+
+        if (Input.GetKeyDown("space"))
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(0, 5, 0);
+        }
+        if (Input.GetKey("left"))
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(-5, 0, 0);
+        }
+        if (Input.GetKey("right"))
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(5, 0, 0);
+        }       
+        if (Input.GetKey("down"))
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, -5);
+        }
+        if (Input.GetKey("up"))
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 5);
+        }       
     }
 }
