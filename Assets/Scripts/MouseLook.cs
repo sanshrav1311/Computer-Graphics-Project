33633e700ef84e558edbacc401cbd5b440dcae7d
sanshrav1311 <sanshrav1311@gmail.com>
2023-03-22 19:56:53 +0530

    Mouse Rotation

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MouseLook : MonoBehaviour
-{
+{   
+    public Vector2 turn;
     void Start()
     {
-
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
+        turn.x += Input.GetAxis("Mouse X");
+        turn.y += Input.GetAxis("Mouse Y");
+        transform.localRotation = Quaternion.Euler(0, turn.x, 0);
     }
 
     void FixedUpdate()
