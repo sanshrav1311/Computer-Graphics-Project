2df5ad508751b5197610dae8987a2a127c5da2e8
sanshrav1311 <sanshrav1311@gmail.com>
2023-03-23 19:48:27 +0530

    enemy movement ai

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,34 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
     public float HP;
-    public float attackRadius = 10f;
+    float attackRadius = 5f;
+    
+    Transform target;
+    NavMeshAgent agent;
     
     void Start()
     {
         HP = 100f;
+        agent = GetComponent<NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     void Update()
     {
-        if(HP <= 0)
+        float distance = Vector3.Distance(target.position, transform.position);
+        if(distance >= attackRadius)
         {
-            Destroy(gameObject);
-        } 
+            agent.SetDestination(target.position);   
+        }
     }
 
     public void TakeDamage(float damage)
     {
         HP -= damage;
+        if(HP <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 
     void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, attackRadius);
+        Gizmos.DrawWireSphere(transform.position, 5f);
     }
 
 }
